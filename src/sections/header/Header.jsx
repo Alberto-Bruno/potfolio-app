203a451560e27ba94f7cadba8f81805a59144523
Alberto-Bruno <brunoalberto97@outlook.it>
2023-08-20 17:00:02 +0200

    Navbar menu Header section

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,33 @@
+import HeaderImage from '../../assets/avatar_8.jpg'
+import data from './data'
 import './header.css'
 
 function Header() {
   return (
-    <header id='header'><h1>Header</h1></header>
+    <header id='header'>
+      <div className="container header-container">
+        <div className='header-profile'>
+          <img src={HeaderImage} alt="" />
+        </div>
+        <h3>dsdgsdg dfgdfg</h3>
+        <p>Lorem ipsum dolor sit, 
+          amet consectetur adipisicing. 
+          Odit magnam incidunt praesentium 
+          autem quod consectetur nisi reiciendis fuga iusto animi, 
+          suscipit provident dolorem facilis laborum 
+          dolorum aperiam laboriosam ducimus sit.
+        </p>
+        <div className='header-cta'>
+          <a href="#contact" className='btn primary'>Conoscimi di più</a>
+          <a href="#portfolio" className='btn light'>Lavoro</a>
+        </div>
+        <div className="header-socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,28 @@
+import Logo from '../../assets/avatar_8.jpg'
+import data from './data'
+import {MdInvertColors} from 'react-icons/md'
 import './navbar.css'
 
 function Navbar() {
   return (
-    <nav><h1>Navbar</h1></nav>
+    <nav>
+      <div className='container nav-container'>
+        <a href="index.html" className='nav-logo'>
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className='nav-menu'>
+          {
+            data.map
+            (item => 
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+            )
+          }
+        </ul>
+          <button id='theme-icon'><MdInvertColors /></button>
+      </div>
+    </nav>
   )
 }
 
